2097804bd5a8fc19c12fa13e2248457f2bfa3d44
Jens Peder Meldgaard <jens.peder.meldgaard@velux.com>
2022-10-09 22:13:07 +0200

    Update colorMap and move getColor to utils

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,41 @@
+const _BUTTON_BACKGROUND = (color) => {
+  return `bg-${color}-400 hover:bg-${color}-600 active:bg-${color}-700 focus:outline-none focus:ring focus:ring-${color}-600`;
+};
+
 export const COLOR_MAP = {
-  green: { background: "bg-green-400", grade: 1 },
-  yellow: { background: "bg-yellow-400", grade: 2 },
-  orange: { background: "bg-orange-400", grade: 3 },
-  blue: { background: "bg-blue-400", grade: 4 },
-  purple: { background: "bg-purple-400", grade: 5 },
-  red: { background: "bg-red-400", grade: 6 },
-  black: { background: "bg-gray-900 opacity-80", grade: 7 },
-  pink: { background: "bg-pink-400", grade: 8 },
+  green: {
+    background: _BUTTON_BACKGROUND("green"),
+    grade: 1,
+  },
+  yellow: {
+    background: _BUTTON_BACKGROUND("yellow"),
+    grade: 2,
+  },
+  orange: {
+    background: _BUTTON_BACKGROUND("orange"),
+    grade: 3,
+  },
+  blue: {
+    background: _BUTTON_BACKGROUND("blue"),
+    grade: 4,
+  },
+  purple: {
+    background: _BUTTON_BACKGROUND("purple"),
+    grade: 5,
+  },
+  red: {
+    background: _BUTTON_BACKGROUND("red"),
+    grade: 6,
+  },
+  black: {
+    background:
+      "bg-gray-700 hover:bg-gray-800 active:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-800",
+    grade: 7,
+  },
+  pink: {
+    background: _BUTTON_BACKGROUND("pink"),
+    grade: 8,
+  },
 };
 
 export const GRIP_TYPES = [
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { COLOR_MAP } from "./constants";
+
 export const guid = () => {
   const CHARS =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -14,4 +16,13 @@ export const toTitleCase = (str: String): String => {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
+};
+
+export const getColor = (color: string) => {
+  const lowerColor = color.toLowerCase();
+  if (lowerColor in COLOR_MAP) {
+    return COLOR_MAP[lowerColor].background;
+  } else {
+    return "bg-white";
+  }
 };
